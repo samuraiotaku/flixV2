38c99a43079a4a1ebe269a3fc8de12c8f2b2fe99
Brandon Shimizu <brandonshimizu@Brandons-MacBook-Pro.local>
2018-09-18 04:24:55 -0700

    Collection View Added

---
+++ flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Brandon Shimizu on 9/18/18.
+//  Copyright © 2018 Brandon Shimizu. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
